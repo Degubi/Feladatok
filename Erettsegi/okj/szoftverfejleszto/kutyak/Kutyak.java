e18c3cc5fa2758eba5358fc1c900821dc8b1f9c0
Degubi <gergonoorbi@gmail.com>
2019-04-14 18:16:06 +0200

    Fresh memes

---
+++ Erettsegi/okj/szoftverfejleszto/kutyak/Kutyak.java
@@ -0,0 +1,122 @@
+import static java.nio.file.StandardOpenOption.*;
+import static java.util.Comparator.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
+import java.util.Map.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class Kutyak {
+
+	//A KutyaFajtak.csv 388. sorában hibás adat van
+	public static void main(String[] args) throws IOException {
+		var nevek = readFile("KutyaNevek.csv", KutyaNev::new, KutyaNev[]::new);
+		var fajtak = readFile("KutyaFajtak.csv", KutyaFajta::new, KutyaFajta[]::new);
+		var kutyik = readFile("Kutyak.csv", Kutya::new, Kutya[]::new);
+		
+		System.out.println("3. Feladat: Kutyanevek száma: " + nevek.length);
+		System.out.print("6. Feladat: Átlag életkor: ");
+		
+		Arrays.stream(kutyik)
+			  .mapToInt(k -> k.eletkor)
+			  .average()
+			  .ifPresent(atlag -> System.out.printf("%.2f év\n", atlag));
+		
+		var legidosebb = Arrays.stream(kutyik).max(comparingInt(k -> k.eletkor)).orElseThrow();
+		System.out.print("7. Feladat: Legidõsebb kutya neve & fajtája: ");
+		System.out.println(KutyaNev.getNevByID(nevek, legidosebb.nevId) + ", " + KutyaFajta.getFajtaByID(fajtak, legidosebb.fajtaId));
+		
+		System.out.println("8. Feladat: 2018 január 10-én vizsgált kutyik:");
+		Arrays.stream(kutyik)
+			  .filter(k -> k.ellenorzes.getYear() == 2018)
+			  .filter(k -> k.ellenorzes.getMonth() == Month.JANUARY)
+			  .filter(k -> k.ellenorzes.getDayOfMonth() == 10)
+			  .collect(Collectors.groupingBy(k -> k.fajtaId, Collectors.counting()))
+			  .forEach((fajtaId, db) -> System.out.println(KutyaFajta.getFajtaByID(fajtak, fajtaId) + ": " + db + " kutya"));
+		
+		System.out.print("9. Feladat: Legjobban leterhelt nap: ");
+		Arrays.stream(kutyik)
+			  .collect(Collectors.groupingBy(k -> k.ellenorzes, Collectors.counting()))
+			  .entrySet().stream()
+			  .max(Entry.comparingByValue())
+			  .ifPresent(k -> System.out.println(k.getKey() + ": " + k.getValue() + " db kutya"));
+		
+		var fileba = Arrays.stream(kutyik)
+						   .map(k -> k.nevId)
+						   .collect(Collectors.toMap(k -> KutyaNev.getNevByID(nevek, k), k -> 1, Integer::sum))
+						   .entrySet().stream()
+						   .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
+						   .map(k -> k.getKey() + ";" + k.getValue())
+						   .collect(Collectors.joining("\n"));
+		
+		Files.writeString(Path.of("nevstatisztika.txt"), fileba, WRITE, TRUNCATE_EXISTING, CREATE);
+	}
+	
+	
+	private static<T> T[] readFile(String fileName, Function<String, T> type, IntFunction<T[]> array) throws IOException {
+		return Files.lines(Path.of(fileName)).skip(1).map(type).toArray(array);
+	}
+	
+	
+	static class KutyaNev{
+		public int id;
+		public String nev;
+		
+		public KutyaNev(String line) {
+			var split = line.split(";");
+			
+			id = Integer.parseInt(split[0]);
+			nev = split[1];
+		}
+		
+		public static String getNevByID(KutyaNev[] nevek, int id) {
+			return Arrays.stream(nevek).filter(k -> k.id == id).map(k -> k.nev).findFirst().orElseThrow();
+		}
+	}
+	
+	static class KutyaFajta{
+		public int id;
+		public String nev;
+		public String eredetiNev;
+		
+		public KutyaFajta(String line) {
+			var split = line.split(";");
+			
+			if(split.length == 2) {
+				System.out.println(line);
+			}
+			
+			id = Integer.parseInt(split[0]);
+			nev = split[1];
+			eredetiNev = split[2];
+		}
+		
+		public static String getFajtaByID(KutyaFajta[] fajtak, int id) {
+			return Arrays.stream(fajtak).filter(k -> k.id == id).map(k -> k.nev).findFirst().orElseThrow();
+		}
+	}
+	
+	static class Kutya{
+		private static final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+		
+		public int id;
+		public int fajtaId;
+		public int nevId;
+		public int eletkor;
+		public LocalDate ellenorzes;
+		
+		public Kutya(String line) {
+			var split = line.split(";");
+			
+			id = Integer.parseInt(split[0]);
+			fajtaId = Integer.parseInt(split[1]);
+			nevId = Integer.parseInt(split[2]);
+			eletkor = Integer.parseInt(split[3]);
+			ellenorzes = LocalDate.parse(split[4], timeFormat);
+		}
+	}
+}
---
+++ Erettsegi/okj/szoftverfejleszto/mukorcsolya/Helsinki2017.java
@@ -0,0 +1,83 @@
+import static java.nio.file.StandardOpenOption.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.Map.*;
+import java.util.stream.*;
+
+public class Helsinki2017 {
+
+	public static void main(String[] args) throws IOException {
+		var versenyzok = Files.lines(Path.of("rovidprogram.csv")).skip(1).map(Versenyzo::new).toArray(Versenyzo[]::new);
+		var dontosok = Files.lines(Path.of("donto.csv")).skip(1).map(Versenyzo::new).toArray(Versenyzo[]::new);
+		
+		System.out.println("2. Feladat: Versenyzõk száma: " + versenyzok.length);
+		System.out.print("3. Feladat: ");
+		
+		Arrays.stream(dontosok)
+			  .filter(k -> k.orszag.equals("HUN"))
+			  .findFirst()
+			  .ifPresentOrElse(k -> System.out.println("A magyar versenyzõ bejutott"), 
+					  		   () -> System.out.println("A magyar versenyzõ nem jutott be"));
+		
+		try(var input = new Scanner(System.in)){
+			System.out.println("5. Feladat: Kérem 1 versenyzõ nevét!");
+			
+			var bekertNev = input.nextLine();
+			
+			System.out.println("6. Feladat: " + bekertNev + " pontszáma: " + osszPontszam(bekertNev, versenyzok, dontosok));
+		}
+		
+		System.out.println("7. Feladat");
+		Arrays.stream(dontosok)
+			  .collect(Collectors.groupingBy(k -> k.orszag, Collectors.counting()))
+			  .entrySet().stream()
+			  .filter(k -> k.getValue() > 1)
+			  .forEach(k -> System.out.println(k.getKey() + ": " + k.getValue() + " db versenyzõ"));
+		
+		
+		//TODO: Ebbe bele kell még számolni a versenyzok tömb pontjait
+		var raw = Arrays.stream(dontosok)
+					   	.collect(Collectors.toMap(k -> k, Versenyzo::sumPont))
+					   	.entrySet().stream()
+					   	.sorted(Entry.comparingByValue(Comparator.reverseOrder()))
+					   	.map(k -> ";" + k.getKey().nev + ";" + k.getKey().orszag + ";" + k.getValue() + "\n")
+					   	.toArray(String[]::new);
+			
+		var fileba = IntStream.range(0, raw.length)
+							  .mapToObj(k -> (k + 1) + raw[k])
+							  .collect(Collectors.joining());
+			
+		Files.writeString(Path.of("vegeredmeny.csv"), fileba, WRITE, CREATE, TRUNCATE_EXISTING);
+	}
+	
+	public static double osszPontszam(String nev, Versenyzo[] versenyzok, Versenyzo[] dontosok) {
+		var versenyPont = Arrays.stream(versenyzok).filter(k -> k.nev.equals(nev)).mapToDouble(Versenyzo::sumPont).sum();
+		var dontosPont = Arrays.stream(dontosok).filter(k -> k.nev.equals(nev)).mapToDouble(Versenyzo::sumPont).sum();
+		
+		return versenyPont + dontosPont;
+	}
+	
+	static class Versenyzo{
+		public String nev;
+		public String orszag;
+		public double techPont;
+		public double kompPont;
+		public int hibaPont;
+		
+		public Versenyzo(String line) {
+			var split = line.split(";");
+			
+			nev = split[0];
+			orszag = split[1];
+			techPont = Double.parseDouble(split[2]);
+			kompPont = Double.parseDouble(split[3]);
+			hibaPont = Integer.parseInt(split[4]);
+		}
+		
+		public double sumPont() {
+			return techPont + kompPont - hibaPont;
+		}
+	}
+}
