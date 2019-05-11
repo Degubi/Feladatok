3a6b705dc8f3d6dc17014c7d99f5ffb94936ae53
Degubi <gergonoorbi@gmail.com>
2019-05-11 22:43:34 +0200

    Update OKJ

---
+++ Erettsegi/okj/rendszerüzemeltetö/hotellift/Lift.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.nio.file.*;
+import java.time.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Lift {
+	static int parseOrDefault(String num, int defaultVal) {
+		try {
+			return Integer.parseInt(num);
+		}catch(NumberFormatException e) {
+			return defaultVal;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		var hasznalatok = Files.lines(Path.of("lift.txt")).map(Hasznalat::new).toArray(Hasznalat[]::new);
+		
+		System.out.println("3. Feladat: Lift alkalmak száma: " + hasznalatok.length);
+		System.out.println("4. Feladat: A korszak " + hasznalatok[0].idopont + " tõl " + hasznalatok[hasznalatok.length - 1].idopont + "-ig tartott");
+		
+		Arrays.stream(hasznalatok)
+			  .max(Comparator.comparingInt(k -> k.kartyaSorszam))
+			  .ifPresent(k -> System.out.println("5. Feladat: Max kártya: " + k.kartyaSorszam + ", celszint: " + k.celSzint));
+		
+		System.out.println("6. Feladat: Írj be egy kártyaszámot és egy célszintet");
+		try(var input = new Scanner(System.in)){
+			var beKartya = parseOrDefault(input.nextLine(), 5);
+			var beCelszint = parseOrDefault(input.nextLine(), 5);
+			
+			var kieg = Arrays.stream(hasznalatok)
+						     .filter(k -> k.kartyaSorszam == beKartya)
+						     .filter(k -> k.celSzint == beCelszint)
+						     .findFirst()
+						     .map(k -> "")
+						     .orElse("nem");
+			
+			System.out.println("7. Feladat: A " + beKartya + " kártyával " + kieg + " utaztak a " + beCelszint + ". emeletre");
+		}
+		
+		System.out.println("8. Feladat");
+		Arrays.stream(hasznalatok)
+			  .collect(Collectors.groupingBy(k -> k.idopont, Collectors.counting()))
+			  .forEach((ido, db) -> System.out.println(ido + " - " + db + "x"));
+	}
+	
+	static class Hasznalat{
+		LocalDate idopont;
+		int kartyaSorszam;
+		int induloSzint;
+		int celSzint;
+		
+		public Hasznalat(String sor) {
+			var split = sor.split(" ");
+			
+			idopont = LocalDate.parse(split[0].replace('.', '-').substring(0, split[0].length() - 1));
+			kartyaSorszam = Integer.parseInt(split[1]);
+			induloSzint = Integer.parseInt(split[2]);
+			celSzint = Integer.parseInt(split[3]);
+		}
+	}
+}
---
+++ Erettsegi/okj/szoftverfejleszto/nobel/Nobel.java
@@ -0,0 +1,67 @@
+import static java.nio.file.StandardOpenOption.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Nobel {
+
+	public static void main(String[] args) throws IOException {
+		var dijak = Files.lines(Path.of("nobel.csv"))
+						 .skip(1)
+						 .map(Dij::new)
+						 .toArray(Dij[]::new);
+		
+		Arrays.stream(dijak)
+			  .filter(k -> k.keresztnev.equals("Arthur B.") && k.vezeteknev.equals("McDonald"))
+			  .findFirst()
+			  .ifPresent(k -> System.out.println("3. Feladat: Arthur díja: " + k.tipus));
+		
+		Arrays.stream(dijak)
+			  .filter(k -> k.evszam == 2017)
+			  .filter(k -> k.tipus.equals("irodalmi"))
+			  .findFirst()
+			  .ifPresent(k -> System.out.println("4. Feladat: Irodalmi dij 2017-ben: " + k.vezeteknev + " " + k.keresztnev));
+		
+		System.out.println("5. Feladat");
+		Arrays.stream(dijak)
+			  .filter(k -> k.vezeteknev.equals(""))
+			  .filter(k -> k.evszam >= 1990)
+			  .forEach(k -> System.out.println(k.evszam + ": " + k.keresztnev));
+		
+		System.out.println("6. Feladat");
+		Arrays.stream(dijak)
+			  .filter(k -> k.vezeteknev.contains("Curie"))
+			  .forEach(k -> System.out.println(k.evszam + ": " + k.vezeteknev + ' ' + k.keresztnev + '(' + k.tipus + ')'));
+		
+		System.out.println("7. Feladat");
+		Arrays.stream(dijak)
+			  .collect(Collectors.groupingBy(k -> k.tipus, Collectors.counting()))
+			  .forEach((dij, db) -> System.out.println(dij + ": " + db + " db"));
+		
+		var orvosi = Arrays.stream(dijak)
+						   .filter(k -> k.tipus.equals("orvosi"))
+						   .sorted(Comparator.comparingInt(k -> k.evszam))
+						   .map(k -> k.evszam + ":" + k.vezeteknev + " " + k.keresztnev)
+						   .collect(Collectors.joining("\n"));
+		
+		Files.writeString(Path.of("orvosi.txt"), orvosi, WRITE, CREATE, TRUNCATE_EXISTING);
+	}
+	
+	static class Dij{
+		int evszam;
+		String tipus;
+		String keresztnev;
+		String vezeteknev;
+		
+		public Dij(String sor) {
+			var split = sor.split(";");
+			
+			evszam = Integer.parseInt(split[0]);
+			tipus = split[1];
+			keresztnev = split[2];
+			vezeteknev = split.length == 4 ? split[3] : "";
+		}
+	}
+}
