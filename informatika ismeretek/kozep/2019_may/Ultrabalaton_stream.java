ff7f4e0b01bbe9f6f94e2c2b35a05493290c4a24
Degubi <gergonoorbi@gmail.com>
2019-12-04 20:22:45 +0100

    2019 may & okt infoismeretek kozep stream megoldasok

---
+++ informatika ismeretek/kozep/2019_may/Ultrabalaton_stream.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Ultrabalaton_stream {
+
+    public static void main(String[] args) throws IOException {
+        var versenyzok = Files.lines(Path.of("ub2017egyeni.txt"))
+                              .skip(1)
+                              .map(Versenyzo::new)
+                              .toArray(Versenyzo[]::new);
+        
+        System.out.println("3. Feladat: Egyéni indulók: " + versenyzok.length);
+        
+        var celbeerkezok = Arrays.stream(versenyzok)
+                                 .filter(k -> k.befejezesSzazalek == 100)
+                                 .toArray(Versenyzo[]::new);
+        
+        var celbaertNoiIndulok = Arrays.stream(celbeerkezok)
+                                       .filter(k -> k.kategoria.equals("Noi"))
+                                       .count();
+        
+        System.out.println("4. Feladat: Célbaért női indulók: " + celbaertNoiIndulok);
+        System.out.println("5. Feladat: Írd be egy versenyző nevét!");
+        
+        try(var console = new Scanner(System.in)){
+            var bekertNev = console.nextLine();
+            
+            Arrays.stream(versenyzok)
+                  .filter(k -> k.nev.equals(bekertNev))
+                  .findFirst()
+                  .ifPresentOrElse(bekert -> {
+                      System.out.println("Indult egyéniben? Igen");
+                      System.out.println("Teljesítette a távot? " + (bekert.befejezesSzazalek == 100 ? "Igen" : "Nem"));
+                  }, () -> System.out.println("Indult egyéniben? Nem"));
+        }
+        
+        Arrays.stream(celbeerkezok)
+              .filter(k -> k.kategoria.equals("Ferfi"))
+              .mapToDouble(Versenyzo::idoOraban)
+              .average()
+              .ifPresent(atlag -> System.out.println("7. Feladat: Átlagos idő: " + atlag + " óra"));
+        
+        Arrays.stream(celbeerkezok)
+              .filter(k -> k.kategoria.equals("Noi"))
+              .min(Comparator.comparingDouble(Versenyzo::idoOraban))
+              .ifPresent(k -> System.out.printf("Nők: %s (%d) - %s\n", k.nev, k.rajtszam, k.ido));
+        
+        Arrays.stream(celbeerkezok)
+              .filter(k -> k.kategoria.equals("Ferfi"))
+              .min(Comparator.comparingDouble(Versenyzo::idoOraban))
+              .ifPresent(k -> System.out.printf("Nők: %s (%d) - %s\n", k.nev, k.rajtszam, k.ido));
+    }
+    
+    public static class Versenyzo {
+        public final String nev;
+        public final int rajtszam;
+        public final String kategoria;
+        public final String ido;
+        public final int befejezesSzazalek;
+        
+        public Versenyzo(String line) {
+            var split = line.split(";");
+            
+            nev = split[0];
+            rajtszam = Integer.parseInt(split[1]);
+            kategoria = split[2];
+            ido = split[3];
+            befejezesSzazalek = Integer.parseInt(split[4]);
+        }
+        
+        public double idoOraban() {
+            var daraboltIdo = ido.split(":");
+            var ora = Double.parseDouble(daraboltIdo[0]);
+            var perc = Double.parseDouble(daraboltIdo[1]);
+            var mp = Double.parseDouble(daraboltIdo[2]);
+            
+            return ora + (perc / 60D) + (mp / 3600D);
+        }
+    }
+}
---
+++ informatika ismeretek/kozep/2019_okt/Cbradio_stream.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.Map.*;
+import java.util.stream.*;
+
+public class Cbradio_stream {
+
+    public static void main(String[] args) throws IOException {
+        var bejegyzesek = Files.lines(Path.of("cb.txt"))
+                               .skip(1)
+                               .map(Bejegyzes::new)
+                               .toArray(Bejegyzes[]::new);
+        
+        System.out.println("3. Feladat: Bejegyzések száma: " + bejegyzesek.length);
+        
+        Arrays.stream(bejegyzesek)
+              .filter(k -> k.adasok == 4)
+              .findFirst()
+              .ifPresentOrElse(k -> System.out.println("4. Feladat: Volt 4 adást indító sofőr"), 
+                              () -> System.out.println("4. Feladat: Nem volt 4 adást indító sofőr"));
+        
+        System.out.println("5. Feladat: Írj be egy nevet");
+        try(var console = new Scanner(System.in)){
+            var bekertNev = console.nextLine();
+            
+            var bekertHasznalatok = Arrays.stream(bejegyzesek)
+                                          .filter(k -> k.nev.equals(bekertNev))
+                                          .mapToInt(k -> k.adasok)
+                                          .sum();
+            
+            System.out.println(bekertNev + " " + bekertHasznalatok + "x használta a rádiót");
+        }
+        
+        var fileHeader = "Kezdes;Nev;AdasDb\n";
+        var fileContent = Arrays.stream(bejegyzesek)
+                                .map(k -> atszamolPercre(k.ora, k.perc) + ";" + k.nev + ";" + k.adasok)
+                                .collect(Collectors.joining("\n"));
+        
+        Files.writeString(Path.of("cb2.txt"), fileHeader + fileContent);
+        
+        var soforokSzama = Arrays.stream(bejegyzesek)
+                                 .map(k -> k.nev)
+                                 .distinct()
+                                 .count();
+        
+        System.out.println("8. Feladat: Sofőrök száma: " + soforokSzama);
+        
+        Arrays.stream(bejegyzesek)
+              .collect(Collectors.groupingBy(k -> k.nev, Collectors.summingInt(k -> k.adasok)))
+              .entrySet().stream()
+              .max(Entry.comparingByValue())
+              .ifPresent(k -> System.out.printf("9. Feladat: Legtöbb adás sofőre: %s: %d db\n", k.getKey(), k.getValue()));
+    }
+    
+    public static int atszamolPercre(int ora, int perc) {
+        return ora * 60 + perc;
+    }
+    
+    public static class Bejegyzes{
+        public final int ora;
+        public final int perc;
+        public final int adasok;
+        public final String nev;
+        
+        public Bejegyzes(String line) {
+            var split = line.split(";");
+            
+            ora = Integer.parseInt(split[0]);
+            perc = Integer.parseInt(split[1]);
+            adasok = Integer.parseInt(split[2]);
+            nev = split[3];
+        }
+    }
+}
