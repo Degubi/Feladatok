9b8e490996d2170566b43e9ab9f4ca29b3d5b772
Degubi <gergonoorbi@gmail.com>
2026-05-20 15:42:13 +0200

    Upload 2025_okt erettsegi kozep

---
+++ erettsegi/kozep/2025_okt/java/Szamlalas.java
@@ -0,0 +1,47 @@
+import java.nio.file.*;
+import java.util.*;
+
+public class Szamlalas {
+
+    public static void main(String[] args) throws Exception {
+        var meresekSzovegek = Files.readString(Path.of("meres.txt")).trim().split(", ");
+        var meresek = new int[meresekSzovegek.length];
+
+        for(var i = 0; i < meresek.length; ++i) {
+            meresek[i] = Integer.parseInt(meresekSzovegek[i]);
+        }
+
+        var osszesKerekparos = 0;
+        for(var meres : meresek) {
+            osszesKerekparos += kerekparosErtekOsszeadashoz(meres);
+        }
+
+        System.out.println("2. Feladat: Összesen " + osszesKerekparos + " kerékpárost számoltak");
+        System.out.println("3. Feladat: Óránkénti mérések:");
+
+        for(int i = 0, ora = 6; i < meresek.length; i += 4, ++ora) {
+            var osszSzam = kerekparosErtekOsszeadashoz(meresek[i]) + kerekparosErtekOsszeadashoz(meresek[i + 1]) + kerekparosErtekOsszeadashoz(meresek[i + 2]) + kerekparosErtekOsszeadashoz(meresek[i + 3]);
+
+            System.out.println(ora + " órától " + osszSzam + " kerékpáros");
+        }
+
+        var maxI = 0;
+        for(var i = 0; i < meresek.length; ++i) {
+            if(meresek[i] > meresek[maxI]) {
+                maxI = i;
+            }
+        }
+
+        System.out.println("Az áthaladók maximális száma: " + meresek[maxI] + ", időpontja: " + idotFormaz(maxI));
+    }
+
+    static int kerekparosErtekOsszeadashoz(int ertek) {
+        return ertek == -1 ? 0 : ertek;
+    }
+
+    static String idotFormaz(int meresI) {
+        var percek = 6 * 60 + 15 + (meresI * 15);
+
+        return (percek / 60) + ":" + (percek % 60);
+    }
+}
---
+++ erettsegi/kozep/2025_okt/java/Szamlalas_stream.java
@@ -0,0 +1,43 @@
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Szamlalas_stream {
+
+    public static void main(String[] args) throws Exception {
+        var meresek = Arrays.stream(Files.readString(Path.of("meres.txt")).trim().split(", "))
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
+
+        var osszesKerekparos = Arrays.stream(meresek)
+                                     .map(Szamlalas_stream::kerekparosErtekOsszeadashoz)
+                                     .sum();
+
+        System.out.println("2. Feladat: Összesen " + osszesKerekparos + " kerékpárost számoltak");
+        System.out.println("3. Feladat: Óránkénti mérések:");
+
+        var orankentiAthaladok = Arrays.stream(meresek)
+                                       .boxed()
+                                       .gather(Gatherers.windowFixed(4))
+                                       .mapToInt(k -> k.stream().mapToInt(Szamlalas_stream::kerekparosErtekOsszeadashoz).sum())
+                                       .toArray();
+
+        IntStream.range(0, orankentiAthaladok.length)
+                 .forEach(i -> System.out.println((i + 6) + " órától " + orankentiAthaladok[i] + " kerékpáros"));
+
+        IntStream.range(0, meresek.length)
+                 .boxed()
+                 .max(Comparator.comparingInt(i -> meresek[i]))
+                 .ifPresent(i -> System.out.println("Az áthaladók maximális száma: " + meresek[i] + ", időpontja: " + idotFormaz(i)));
+    }
+
+    static int kerekparosErtekOsszeadashoz(int ertek) {
+        return ertek == -1 ? 0 : ertek;
+    }
+
+    static String idotFormaz(int meresI) {
+        var percek = 6 * 60 + 15 + (meresI * 15);
+
+        return (percek / 60) + ":" + (percek % 60);
+    }
+}
